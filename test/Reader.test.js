6961628d3481a30c55b448be4082ce7c8d07303e
FatimaGloria <janettgloria1@gmail.com>
2022-05-25 23:50:05 -0500

    6.1 Añadiendo prueba unitaria (pausa para investigar)

---
+++ test/Reader.test.js
@@ -0,0 +1,9 @@
+const Reader = require("./../lib/utils/Reader");
+
+describe("Pruebas unitarias para explorers_launchx", () => {
+    test('1. Leer el archivo y regresar la información', () => {
+        
+        
+        expect().toBe();
+    });
+})
